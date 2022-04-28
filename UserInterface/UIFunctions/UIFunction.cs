d791e6ab9be27d3e2e956bf00256e9575c18d805
ArmMic <arman.harutyunyan.1996@mail.ru>
2022-04-28 17:09:36 +0400

    Add functions for user interface

---
+++ UserInterface/UIFunctions/UIFunction.cs
@@ -0,0 +1,26 @@
+﻿namespace UserInterface.UIFunctions
+{
+    public static class UIFunction
+    {
+        public static void Write(string text, bool isNewLine = false)
+        {
+            Console.Write(text);
+            if (isNewLine)
+            {
+                Console.WriteLine();
+            }
+        }
+        public static string Read()
+        {
+            return Console.ReadLine();
+        }
+        public static void Clear()
+        {
+            Console.Clear();
+        }
+        public static ConsoleKey GetKey()
+        {
+            return Console.ReadKey().Key;
+        }
+    }
+}
