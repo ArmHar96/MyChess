3e9548d9e8c94b8945a39739ca3e0ef732127cd4
ArmMic <arman.harutyunyan.1996@mail.ru>
2022-04-28 17:14:01 +0400

    Add some code to run the view

--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using UserInterface.UserView;
+
+ChessView chessView = new ChessView();
+chessView.Start();
