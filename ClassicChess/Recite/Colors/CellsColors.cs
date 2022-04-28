45c3deaf1813c1fb97f9dbab45b64f5b7c2aa8be
ArmMic <arman.harutyunyan.1996@mail.ru>
2022-04-28 16:58:47 +0400

    Add project files.

---
+++ ClassicChess/Recite/Colors/CellsColors.cs
@@ -0,0 +1,13 @@
+﻿namespace Modules.Recite.Colors
+{
+    /// <summary>
+    /// Specifies constants that define background colors for the cells.
+    /// </summary>
+    public enum CellsColors
+    {
+        White = 15,
+        Black = 0,
+        DarkYellow = 6,
+        Blue = 9
+    }
+}
